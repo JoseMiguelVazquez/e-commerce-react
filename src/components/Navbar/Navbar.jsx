e5cc2c3b37528e51fa5dbc4080013066efd3ebb6
JoseMiguelVazquez <jmiguel_1991@hotmail.com>
2023-04-24 18:44:21 -0600

    nav colors and styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import './navbar.css'
 
 const Navbar = () => {
   return (
-    <nav className='navbar navbar-expand-md bg-body-tertiary mb-3'>
+    <nav className='navbar navbar-expand-md bg-body-tertiary navbar-dark'>
       <div className='container-fluid'>
         <NavLink className='navbar-brand' to='#'>Navbar</NavLink>
         <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
@@ -23,7 +24,7 @@ const Navbar = () => {
           </ul>
           <form className='d-flex' role='search'>
             <input className='form-control me-2' type='search' placeholder='Search' aria-label='Search' />
-            <button className='btn btn-outline-success' type='submit'>Search</button>
+            <button className='btn btn-custom' type='submit'>Search</button>
           </form>
           <ul className='navbar-nav mb-2 mb-lg-0'>
             <li className='nav-item dropdown'>
