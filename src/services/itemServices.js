be40f58b21264d14b05412fc0807b4611bdd6cde
JoseMiguelVazquez <jmiguel_1991@hotmail.com>
2023-05-04 22:09:52 -0600

    ✨ itemServices

---
+++ src/services/itemServices.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+const BASE_URL = 'http://localhost:3000'
+
+// axios.interceptors.request.use((config) => {
+//   const token = window.localStorage.getItem('token')
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`
+//   }
+//   return config
+// }, (error) => {
+//   return Promise.reject(error)
+// })
+
+const getSingleItem = (id) => axios.get(`${BASE_URL}/items/${id}`)
+const getAllItems = () => axios.get(`${BASE_URL}/items`)
+
+export {
+  getSingleItem,
+  getAllItems
+}
