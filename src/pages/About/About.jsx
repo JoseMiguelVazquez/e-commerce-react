ef6106e671da7e14e0628f96cf3eaef7b151753b
JoseMiguelVazquez <jmiguel_1991@hotmail.com>
2023-05-07 23:12:56 -0600

    about page and styles

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import './about.css'
 
 const About = () => {
   return (
     <>
-      <div className='container'>
-        <a href='https://www.vecteezy.com/free-vector/ecommerce-banner' target='_blank' rel='noreferrer'>Ecommerce Banner Vectors by Vecteezy</a>
+      <div className='container mt-5 about-section'>
+        <h1>About this Project</h1>
+        <p>This app was made with React and Bootstrap using a <a target='_blank' href='https://github.com/warderer/json-server-jwt/blob/main/db.json' rel='noreferrer'>local API made by Cesar Guerra</a></p>
+        <p>Tab Icon made from <a target='_blank' href='http://www.onlinewebfonts.com/icon' rel='noreferrer'>Icon Fonts</a> is licensed by CC BY 3.0</p>
+        <a target='_blank' href='https://www.vecteezy.com/free-vector/ecommerce-banner' rel='noreferrer'>Ecommerce Banner Vectors by Vecteezy</a>
+        <p className='mt-5'>Developed by <a target='_blank' href='https://github.com/JoseMiguelVazquez' rel='noreferrer'>José Miguel Vázquez E.</a></p>
       </div>
     </>
   )
